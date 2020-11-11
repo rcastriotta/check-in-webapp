9de9f2b7a7aef4f8f4c7ede1de630ae19164bb48
rcastriotta <63270523+rcastriotta@users.noreply.github.com>
2020-11-11 16:20:34 -0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,14 +4,7 @@ import 'firebase/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAv773xEN_r3_yYHppzrcwA601d_kYGffI",
-    authDomain: "checkin-e8bea.firebaseapp.com",
-    databaseURL: "https://checkin-e8bea.firebaseio.com",
-    projectId: "checkin-e8bea",
-    storageBucket: "checkin-e8bea.appspot.com",
-    messagingSenderId: "683436459496",
-    appId: "1:683436459496:web:6d2225d9844286bb46905d",
-    measurementId: "G-D8V9MBGRDJ"
+    // FIREBASE PROJECT CONFIG HERE
 };
 
 firebase.initializeApp(firebaseConfig);
